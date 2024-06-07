e1dd9f790f3aafde151c78d5cf4d48b914602afb
Vaggelis Magonezos <nospipian@gmail.com>
2024-06-07 15:26:20 +0300

    Update models.js

--- models/models.js
+++ models/models.js
@@ -547,17 +547,22 @@ const scheduleTaskSchema = new Schema(
 scheduleTaskSchema.plugin(mongoosePaginate)
 scheduleTaskSchema.plugin(mongooseAggregatePaginate)
 
-const taskSchema = new Schema({
-  product: { type: String, required: true },
-  option_id: String,
-  date: { type: String, required: true },
-  assignees: Array,
-  vehicle_id: String,
-  pickups: [pickupSchema],
-  details: String,
-  tour_group_id: String,
-  author_id: { type: String, required: true },
-})
+const taskSchema = new Schema(
+  {
+    product: { type: String, required: true },
+    date: { type: String, required: true },
+    assignees: Array,
+    vehicle_id: String,
+    pickups: [pickupSchema],
+    details: String,
+    tour_group_id: String,
+    author_id: { type: String, required: true },
+  },
+  {
+    minimize: false,
+    //allows to save empty objects in db
+  }
+)
 
 taskSchema.plugin(mongoosePaginate)
 taskSchema.plugin(mongooseAggregatePaginate)
