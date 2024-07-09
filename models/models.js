ab85cecd33df3f3c14b483a580e5e9bde1904819
Vaggelis Magonezos <nospipian@gmail.com>
2024-07-09 17:19:04 +0300

    Update models.js

--- models/models.js
+++ models/models.js
@@ -653,7 +653,7 @@ scheduleTaskSchema.plugin(mongooseAggregatePaginate);
 const taskSchema = new Schema(
   {
     product: { type: String, required: true },
-    option_id: { type: String, required: true },
+    option_id: { type: String },
     date: { type: String, required: true },
     assignees: Array,
     vehicle_id: String,
