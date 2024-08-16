b3a7583645983a133ddf4b895beae615072250ca
Vaggelis Magonezos <nospipian@gmail.com>
2024-08-16 14:48:48 +0300

    Update models.js

--- models/models.js
+++ models/models.js
@@ -819,6 +819,7 @@ const walletSchema = new Schema(
 const categorySchema = new Schema(
   {
     title: { type: String, required: true },
+    user: { type: String, required: true },
   },
   { timestamps: true }
 )
