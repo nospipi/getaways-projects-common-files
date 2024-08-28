e764962ef9c02b36ac6ed90aafaae3432edfb1f8
Vaggelis Magonezos <nospipian@gmail.com>
2024-08-28 16:03:26 +0300

    Update models.js

--- models/models.js
+++ models/models.js
@@ -765,7 +765,9 @@ vehicleServiceLogEntrySchema.plugin(mongoosePaginate)
 const bokunDataSchema = new Schema({
   action: String,
   ref: String,
-  data: Object,
+  data: {
+    type: Schema.Types.Mixed, // Allows any data type
+  },
   date: {
     type: String,
     default: moment().format("YYYY-MM-DD HH:mm:ss"),
